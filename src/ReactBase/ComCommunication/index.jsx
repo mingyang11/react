3946727d5e10438c0ec2dc1bd91672667efe2314
yangmy <yangmy@servyou.com.cn>
2023-06-12 09:54:00 +0800

    first commit

---
+++ src/ReactBase/ComCommunication/index.jsx
@@ -0,0 +1,7 @@
+/**
+ * 1.组件通信方式、强化方式
+ */
+function ComCommunication() {
+  return 'ComCommunication';
+}
+export default ComCommunication;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import ComCommunication from '../ReactBase/ComCommunication/index.jsx';
+import styles from './index.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <ComCommunication />
+    </div>
+  );
+}
